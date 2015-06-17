b2f0761c62f0d70ee231cb2c01d0d5cf37875b13
bksk1lz <bcking21@gmail.com>
2015-06-17 10:08:19 -0400

    week 6 results

--- 2015/lister2.py
+++ 2015/lister2.py
@@ -7,8 +7,6 @@ for file in os.listdir(os.getcwd()):
     if file.endswith(".xml"):
         filelist.append(file)
 
-namelist = [{}] #Initialize dictionary list of names and race results
-
 
 pointslist = [100, 90, 81, 73, 66, 60, 55, 51, 47, 44, 41, 39, 37, 36, 35, 34,
               33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18,
@@ -16,8 +14,17 @@ pointslist = [100, 90, 81, 73, 66, 60, 55, 51, 47, 44, 41, 39, 37, 36, 35, 34,
 pointslist2 = [10] * 60
 pointslist.extend(pointslist2)
 pointslist.append(5)
+pointslist.reverse()
 
 racenumlist = ["name"]
+
+namelistm = [{}] #Initialize dictionary list of names and race results
+namelistw = [{}]
+
+#Get the list of women names
+women = open('women.txt', 'r')
+womenlist = women.read().split('\n')
+women.close()
 
 import xml.etree.ElementTree as ET
 
@@ -28,43 +35,65 @@ for i, file in enumerate(filelist):
     racenumstr = "race" + str(i+1)
     racenumlist.append(racenumstr)
     
+    pointslistm = list(pointslist)
+    pointslistw = list(pointslist)
+
     for ClassResult in root.find("ClassResult"):
         #iterate over all the person results
         for PersonResult in ClassResult.iter("PersonResult"):
             namestr = ""
             x = 0 # A variable to keep track of whether the runner's name already exists in namelist
+
             
             for Given in PersonResult.iter("Given"):
                 namestr = namestr + Given.text + " "
             
             for Family in PersonResult.iter("Family"):
                 namestr = namestr + Family.text #namestr now contains entire name of person
-            
-            for ResultPosition in PersonResult.iter("ResultPosition"):
-                raceposval = int(ResultPosition.text) #raceposval contains place for this race
-            
-            for entry in namelist:
-                #search namelist for prior entry with the same name
+                     
+            if namestr in womenlist:
                 
-                if namestr in entry.values(): #if there is one, add raceposval to the dictionary
-                    entry[racenumstr] = pointslist[raceposval - 1]
-                    x = 1
+                for entry in namelistw:
+                
+                    #search namelist for prior entry with the same name
+                    if namestr in entry.values(): #if there is one, add raceposval to the dictionary
+                        entry[racenumstr] = pointslistw.pop()
+                        x = 1
                     
-            if x == 0:
-                #if not, append namelist with a new entry, including name and race position
-                namelist.append({"name":namestr, 
-                                racenumstr:pointslist[raceposval - 1]})
+                if x == 0:
+                    #if not, append namelist with a new entry, including name and race position
+                    namelistw.append({"name":namestr, 
+                                racenumstr:pointslistw.pop()})
                 
-            raceposval = 100
+            else:
+                for entry in namelistm:
+                
+                    #search namelist for prior entry with the same name
+                    if namestr in entry.values(): #if there is one, add raceposval to the dictionary
+                        entry[racenumstr] = pointslistm.pop()
+                        x = 1
+                    
+                if x == 0:
+                    #if not, append namelist with a new entry, including name and race position
+                    namelistm.append({"name":namestr, 
+                                racenumstr:pointslistm.pop()})
 
 from operator import itemgetter
             
-for entry in namelist:
-    best10 = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:11])
+for entry in namelistm:
+    best10m = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:11])
     # sorts all dictionary values into a list. The text entry (person's name) is first
     # the best 10 points values are 1 - 11 in the list. they are stored in the dictionary
     # best10
-    entry['sumbest10'] = sum(best10.itervalues())
+    entry['sumbest10'] = sum(best10m.itervalues())
+    #sum of the 10 best entries is added to each runners dictionary under the entry "sumbest10"
+
+for entry in namelistw:
+    best10w = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:11])
+    # sorts all dictionary values into a list. The text entry (person's name) is first
+    # the best 10 points values are 1 - 11 in the list. they are stored in the dictionary
+    # best10
+    entry['sumbest10'] = sum(best10w.itervalues())
     #sum of the 10 best entries is added to each runners dictionary under the entry "sumbest10"
 
 racenumlist.append('sumbest10') 
@@ -76,6 +105,8 @@ outfile = open("output.csv", "w")
 w = csv.DictWriter(outfile, fieldnames = racenumlist, 
                    restval=0, extrasaction='ignore', dialect='excel')
 
-for entry in namelist:
-	w.writerow(entry)
+for entry in namelistm:
+    w.writerow(entry)
+for entry in namelistw:
+    w.writerow(entry)
 outfile.close()
