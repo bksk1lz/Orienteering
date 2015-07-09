739f46b24c5d6c4a6032b9de2db90c3a5349a99a
bksk1lz <bcking21@gmail.com>
2015-07-09 10:47:47 -0400

    code update and results
    made MPs now worth 10 points.
    commented code
    
    3 weeks of results in here too

--- 2015/lister2.py
+++ 2015/lister2.py
@@ -8,99 +8,135 @@ for file in os.listdir(os.getcwd()):
         filelist.append(file)
 
 
+#This is an awkward way of building the list of point values to awards to each runner
+#1st value in the list is points for 1st place and so on
 pointslist = [100, 90, 81, 73, 66, 60, 55, 51, 47, 44, 41, 39, 37, 36, 35, 34,
               33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18,
-              17, 16, 15, 14, 13, 12, 11]
-pointslist2 = [10] * 60
+              17, 16]
+pointslist2 = [15] * 65
 pointslist.extend(pointslist2)
 pointslist.append(5)
 pointslist.reverse()
 
-racenumlist = ["name"]
 
-namelistm = [{}] #Initialize dictionary list of names and race results
-namelistw = [{}]
+
+#From here on we are doing mens and womens results in parallel
+namelistm = [{}] #Initialize dictionary list of men's names and race results
+namelistw = [{}] ##Initialize dictionary list of women's names and race results
 
 #Get the list of women names
+#women.txt has a woman runner's name on each line
+#It is read into a list of the name strings
 women = open('women.txt', 'r')
 womenlist = women.read().split('\n')
 women.close()
 
+#Now we are ready to go through the results files and write everyone's point values
+#to their entry in namelist
 import xml.etree.ElementTree as ET
+racenumlist = ["name"]
 
 # Load each file and make string for the race number
 for i, file in enumerate(filelist):
     tree = ET.parse(file)
     root = tree.getroot()
     racenumstr = "race" + str(i+1)
-    racenumlist.append(racenumstr)
+    racenumlist.append(racenumstr)#builds a list of strings corresponding to the sequence of races
     
+    #Reinitialize points list for men and women ever time thru this loop because we 
+    #pop off values to write points into namelist
     pointslistm = list(pointslist)
     pointslistw = list(pointslist)
-
+    
+    #take only the first class result in file. (sometimes there's a reverse course or something at the end)
     for ClassResult in root.find("ClassResult"):
-        #iterate over all the person results
+        #Now we're in a loop iterating through the person results
         for PersonResult in ClassResult.iter("PersonResult"):
             namestr = ""
             x = 0 # A variable to keep track of whether the runner's name already exists in namelist
 
-            
+            #Write the runners name to namestr
             for Given in PersonResult.iter("Given"):
                 namestr = namestr + Given.text + " "
-            
+                            
             for Family in PersonResult.iter("Family"):
-                namestr = namestr + Family.text #namestr now contains entire name of person
+                namestr = namestr + Family.text 
+                
+            #Write resultposition to rpos
+            for ResultPosition in PersonResult.iter("ResultPosition"):
+                rpos = ResultPosition.text
                      
+            #Fist check women list to see if runner is a woman.
             if namestr in womenlist:
                 
                 for entry in namelistw:
                 
-                    #search namelist for prior entry with the same name
+                    #iterate thru namelist for prior entry with the same name
                     if namestr in entry.values(): #if there is one, add raceposval to the dictionary
+                        #pop from pointslistw to get points value because results file lists
+                        #runners in finishing order
                         entry[racenumstr] = pointslistw.pop()
+                        #if the runner mispunched, they were still rewarded the next poins in pointslistw
+                        #but we need to give them 10. So check rpos, which will be None if runner MP
+                        if rpos is None:
+                            entry[racenumstr] = 10
                         x = 1
                     
                 if x == 0:
                     #if not, append namelist with a new entry, including name and race position
                     namelistw.append({"name":namestr, 
                                 racenumstr:pointslistw.pop()})
+                    #do MP check again
+                    if rpos is None:
+                            entry[racenumstr] = 10
                 
+            #if not a woman, do the same thing but in namelistm
             else:
                 for entry in namelistm:
                 
-                    #search namelist for prior entry with the same name
-                    if namestr in entry.values(): #if there is one, add raceposval to the dictionary
+                    if namestr in entry.values(): 
                         entry[racenumstr] = pointslistm.pop()
+                        if rpos is None:
+                            entry[racenumstr] = 10
                         x = 1
                     
                 if x == 0:
-                    #if not, append namelist with a new entry, including name and race position
                     namelistm.append({"name":namestr, 
                                 racenumstr:pointslistm.pop()})
+                    if rpos is None:
+                            entry[racenumstr] = 10
+            
+            #have to reset ResultPosition and rpos, otherwise old values will still be there and
+            #the MP points won't get written
+            ResultPosition.clear()
+            rpos = None
 
+#Men's and women's dictionaries now full of everybody's names and points for each race
+#we need to get their best 11 scores
 from operator import itemgetter
             
 for entry in namelistm:
-    best10m = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:11])
+    best11m = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:12])
     # sorts all dictionary values into a list. The text entry (person's name) is first
-    # the best 10 points values are 1 - 11 in the list. they are stored in the dictionary
+    # the best 11 points values are 1 - 12 in the list. they are stored in the dictionary
     # best10
-    entry['sumbest10'] = sum(best10m.itervalues())
-    #sum of the 10 best entries is added to each runners dictionary under the entry "sumbest10"
+    entry['sumbest11'] = sum(best11m.itervalues())
+    #sum of the 11 best entries is added to each runners dictionary under the entry "sumbest11"
 
+#Now do the same thing for women's
 for entry in namelistw:
-    best10w = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:11])
-    # sorts all dictionary values into a list. The text entry (person's name) is first
-    # the best 10 points values are 1 - 11 in the list. they are stored in the dictionary
-    # best10
-    entry['sumbest10'] = sum(best10w.itervalues())
-    #sum of the 10 best entries is added to each runners dictionary under the entry "sumbest10"
+    best11w = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:12])
+    entry['sumbest11'] = sum(best11w.itervalues())
+    
 
-racenumlist.append('sumbest10') 
-#adds the sumbest10 entry title to racenumlist for inclusion in csv output
+racenumlist.append('sumbest11') 
+#adds the sumbest11 entry title to racenumlist for inclusion in csv output
+#csv header is not working right now for some reason
 
 import csv
 #write output .csv file.
+#men's and women's dicts are just written consecutively for now
+#would like to write this direct to google drive someday!
 outfile = open("output.csv", "w")
 w = csv.DictWriter(outfile, fieldnames = racenumlist, 
                    restval=0, extrasaction='ignore', dialect='excel')
